4bc1edac21408a2714605b5fe0f8279c75693342
root <cauwulixuan@163.com>
2018-01-04 10:37:36 +0800

    add k8s heapster images...

--- k8s_image_download.sh
+++ k8s_image_download.sh
@@ -6,6 +6,10 @@ KUBE_PAUSE_VERSION=3.0
 ETCD_VERSION=3.0.17
 DNS_VERSION=1.14.5
 DASHBOARD_VERSION=v1.8.0
+GRAFANA_VERSION=v4.4.3
+HEAPSTER_VERSION=v1.4.2
+INFLUXDB_VERSION=v1.3.3
+
 ARCH=amd64
 
 QUAY_URL=quay.io/coreos
@@ -23,15 +27,23 @@ k8s.gcr.io/k8s-dns-sidecar-${ARCH}:${DNS_VERSION}
 k8s.gcr.io/k8s-dns-kube-dns-${ARCH}:${DNS_VERSION}
 k8s.gcr.io/k8s-dns-dnsmasq-nanny-${ARCH}:${DNS_VERSION}
 k8s.gcr.io/kubernetes-dashboard-${ARCH}:${DASHBOARD_VERSION}
+k8s.gcr.io/heapster-grafana-${ARCH}:${GRAFANA_VERSION}
+k8s.gcr.io/heapster-${ARCH}:${HEAPSTER_VERSION}
+k8s.gcr.io/heapster-influxdb-${ARCH}:${INFLUXDB_VERSION}
 quay.io/coreos/flannel:v0.8.0-amd64
 )
 
 for imageName in ${image_list[@]}; 
 do
   dir=`echo ${imageName} | awk -F ":" '{print $1}'|awk -F "/" '{print $2}'`
-  mkdir ${dir}
+  mkdir -p ${dir}
   cat <<EOF > ${dir}/Dockerfile
   FROM ${imageName}
   MAINTAINER wulixuan <wulixuan@inspur.com>
 EOF
 done
+
+git add .
+git commit -m "add k8s heapster images..."
+git push origin master
+
